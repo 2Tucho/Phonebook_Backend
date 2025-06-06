eac36231992cc3a2c067910f8714a9636c650e33
2Tucho <albergromero@gmail.com>
2025-06-06 12:52:08 +0200

    Added exercise 3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const morgan = require("./morgan")
+const Person = require('./models/person')
 const cors = require('cors')
 
 app.use(cors())
@@ -12,32 +14,35 @@ app.use(express.json())
 app.use(morgan(':method :url :status - :res[content-length] :response-time ms :body'));
 
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         "id": 1,
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": 2,
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": 4,
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 //GET ALL PERSONS http://localhost:3001/api/persons
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    console.log("hola")
     response.json(persons)
+  })
 })
 
 //GET INFO http://localhost:3001/info
@@ -95,7 +100,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
